a65cbf46b44a37396da56bfb06c6ab93cb558389
ahawkins <me@broadcastingadam.com>
2013-06-05 08:57:28 +0200

    Expire cookies

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TweetClock::Application.config.secret_token = '7253f5a10797f9c7f3a00f0a4c3c59506f1da3faa61d3a33a3510991250511e0f0f31612597ab94b1916d94500ca51826134f18292cfb3f7dc3469cfa9c03301'
+TweetClock::Application.config.secret_token = '3cae0be6ddd0eb201729ac21d58959a4a5db046229ec97a85afb71858c0007903f80e0b5d31c3dd8fbd5d8744bd7de98a28d52717e9e7b88765a440ed9f68ddf'
