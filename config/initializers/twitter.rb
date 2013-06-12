cb91466129385e575c111a8ef9590d09c061a22a
Benjamin Silas Rhodes <benjamin.s.rhodes@gmail.com>
2013-06-12 10:22:31 -0700

    raise exception when required twitter configuration is not present

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
 Twitter.configure do |config|
-  config.consumer_key = ENV['TWITTER_KEY']
-  config.consumer_secret = ENV['TWITTER_SECRET']
+  config.consumer_key = ENV.fetch('TWITTER_KEY')
+  config.consumer_secret = ENV.fetch('TWITTER_SECRET')
 end
